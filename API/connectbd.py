c2b906cbd11ee135fb10023636b8f122f8839c9b
jcjean <jeancharlessjesus@gmail.com>
2023-09-28 23:21:01 -0300

    Configuraçoes do banco de dados

---
+++ API/connectbd.py
@@ -0,0 +1,7 @@
+# Configuração do banco de dados PostgreSQL
+bd_config= {
+    'host': 'mylibdatabase.c9bopnmijrhe.us-east-1.rds.amazonaws.com',
+    'dbname': 'postgres',
+    'user': 'libadmin',
+    'password': 'libadmin',
+    'port': '5432'}
