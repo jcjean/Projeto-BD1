fc921c4d1fa1be6bdd16318621e68afaf2cf8c01
jcjean <jeancharlessjesus@gmail.com>
2023-10-03 19:14:40 -0300

    update database

--- API/connectbd.py
+++ API/connectbd.py
@@ -1,7 +1,7 @@
 # Configuração do banco de dados PostgreSQL
 bd_config= {
     'host': 'database-1.cqwekq8fdzrb.us-east-1.rds.amazonaws.com',
-    'dbname': 'database-1',
+    'dbname': 'postgres',
     'user': 'jean',
     'password': 'professor',
     'port': '5432'}
